45531342953c9475b9103a3c2a05aa661ad34702
Grabitel1605 <55201352+grabitel1605@users.noreply.github.com>
2022-08-09 20:30:25 -0500

    Add files via upload

---
+++ scripts/powershell/keepalive.ps1
@@ -0,0 +1,14 @@
+﻿echo "
+ +-+-+-+-+ +-+-+-+-+-+-+
+ |K|e|e|p| |A|l|i|v|e|!|
+ +-+-+-+-+ +-+-+-+-+-+-+"
+$WShell = New-Object -com "Wscript.shell"
+
+while ($True)
+
+{
+$Wshell.sendkeys("{SCROLLLOCK}")
+Start-Sleep -Milliseconds 100
+$Wshell.sendkeys("{SCROLLLOCK}")
+Start-Sleep -Seconds 240
+}
